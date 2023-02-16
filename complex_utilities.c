611944e004d7dedec993eecec1a02c0032525062
Luis Sileoni <lsileoni@c2r1p1.hive.fi>
2023-02-16 19:43:04 +0200

    Spaces between operators for readability

--- complex_utilities.c
+++ complex_utilities.c
@@ -18,11 +18,11 @@ t_complex	com_mul(t_complex a, t_complex b)
     return (z);
 }
 
-t_complex	com_div(t_complex a,t_complex b)
+t_complex	com_div(t_complex a, t_complex b)
 {
     t_complex z;
 
-    z.r = (a.r*b.r + a.i*b.i)/(b.r*b.r+b.i*b.i);
-    z.i = (a.i*b.r - a.r*b.i)/(b.r*b.r + b.i*b.i);
+    z.r = (a.r * b.r + a.i * b.i) / (b.r * b.r + b.i * b.i);
+    z.i = (a.i * b.r - a.r * b.i) / (b.r * b.r + b.i * b.i);
     return (z);
 }
