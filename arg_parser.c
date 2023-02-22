cd947632277c48c2dfdd5d01c4b5550df908841f
lsileoni <lsileoni@proton.me>
2023-02-22 23:18:10 +0200

    Fixed program not printing help message.

--- arg_parser.c
+++ arg_parser.c
@@ -6,11 +6,22 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 07:00:52 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/02/22 22:15:26 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/02/22 23:14:37 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void	print_help(void)
+{
+	ft_printf("\nProgram usage\n-------------------------\n");
+	ft_printf("Static fractals:  ./fractol {mandelbrot/julia}\n");
+	ft_printf("Mandelbrot parameters: {width height}\n");
+	ft_printf("Julia parameters: {c.r c.i width height}\n\n");
+	ft_printf("Examples\n-------------------------\n");
+	ft_printf("./fractol julia 0.8 0.0 800 800\n");
+	ft_printf("./fractol mandelbrot 800 800\n");
+}
 
 static int	check_six(t_args *args, t_set *set, char **argv)
 {
@@ -25,6 +36,7 @@ static int	check_six(t_args *args, t_set *set, char **argv)
 	else
 	{
 		ft_printf("A parameter count of 4 only works with a Julia set\n");
+		print_help();
 		return (0);
 	}
 }
@@ -64,21 +76,12 @@ static int	check_two_up(t_args *args, char **argv, int argc, t_set *set)
 	else
 	{
 		ft_printf("Invalid number of parameters!\n");
+		print_help();
 		return (0);
 	}
 	return (1);
 }
 
-static void	print_help(void)
-{
-	ft_printf("\nProgram usage\n-------------------------\n");
-	ft_printf("Static fractals:  ./fractol {mandelbrot/julia}\n");
-	ft_printf("Mandelbrot parameters: {width height}\n");
-	ft_printf("Julia parameters: {c.r c.i width height}\n\n");
-	ft_printf("Examples\n-------------------------\n");
-	ft_printf("./fractol julia 0.8 0.0 800 800\n");
-	ft_printf("./fractol mandelbrot 800 800\n");
-}
 
 int	argument_parser(int argc, char **argv, t_args *args, t_set *set)
 {
@@ -92,6 +95,7 @@ int	argument_parser(int argc, char **argv, t_args *args, t_set *set)
 	if (!init_set(argv[1], set))
 	{
 		ft_printf("Set name is wrong!\n");
+		print_help();
 		return (0);
 	}
 	if (!check_two_up(args, argv, argc, set))
--- arg_parser_bonus.c
+++ arg_parser_bonus.c
@@ -6,11 +6,24 @@
 /*   By: lsileoni <lsileoni@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 07:00:52 by lsileoni          #+#    #+#             */
-/*   Updated: 2023/02/22 22:26:59 by lsileoni         ###   ########.fr       */
+/*   Updated: 2023/02/22 23:15:30 by lsileoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol_bonus.h"
+
+static void	print_help(void)
+{
+	ft_printf("\nProgram usage\n-------------------------\n");
+	ft_printf("Static fractals:  ./fractol {mandelbrot/burningship}");
+	ft_printf(" \t\t{optional x and y resolution}\n");
+	ft_printf("Dynamic fractals: ./fractol {julia} {optional r and i coords}");
+	ft_printf("   {optional x and y resolution}\n\n");
+	ft_printf("Examples\n-------------------------\n");
+	ft_printf("./fractol julia 0.8 0.0 800 800\n");
+	ft_printf("./fractol mandelbrot 800 800\n");
+	ft_printf("./fractol burningship\n\n");
+}
 
 static int	check_six(t_args *args, t_set *set, char **argv)
 {
@@ -26,6 +39,7 @@ static int	check_six(t_args *args, t_set *set, char **argv)
 	else
 	{
 		ft_printf("A parameter count of 4 only works with a Julia set\n");
+		print_help();
 		return (0);
 	}
 }
@@ -64,22 +78,10 @@ static int	check_two_up(t_args *args, char **argv, int argc, t_set *set)
 	else
 	{
 		ft_printf("Invalid number of parameters!\n");
+		print_help();
 		return (0);
 	}
 	return (1);
-}
-
-static void	print_help(void)
-{
-	ft_printf("\nProgram usage\n-------------------------\n");
-	ft_printf("Static fractals:  ./fractol {mandelbrot/burningship}");
-	ft_printf(" \t\t{optional x and y resolution}\n");
-	ft_printf("Dynamic fractals: ./fractol {julia} {optional r and i coords}");
-	ft_printf("   {optional x and y resolution}\n\n");
-	ft_printf("Examples\n-------------------------\n");
-	ft_printf("./fractol julia 0.8 0.0 800 800\n");
-	ft_printf("./fractol mandelbrot 800 800\n");
-	ft_printf("./fractol burningship\n\n");
 }
 
 int	argument_parser(int argc, char **argv, t_args *args, t_set *set)
@@ -95,6 +97,7 @@ int	argument_parser(int argc, char **argv, t_args *args, t_set *set)
 	if (!init_set(argv[1], set))
 	{
 		ft_printf("Set name is wrong!\n");
+		print_help();
 		return (0);
 	}
 	if (!check_two_up(args, argv, argc, set))
